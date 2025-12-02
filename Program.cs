acc6bf88bbaec258b304ab6f5740df37af023f27
Amaro Netto <amarosilva002@gmail.com>
2025-12-02 00:12:34 +0000

    "Desafio concluido: CRUD de Tarefas implementado com sucesso"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConexaoPadrao"))); //torca do SQLServer pelo SQLlite, já que estou usando o codespace.
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
